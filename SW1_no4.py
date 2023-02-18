b34eedf3998e1cf12ddc5e8b8177357a95455a76
MartinCannotCode <107410359+MartinCannotCode@users.noreply.github.com>
2023-02-18 09:12:11 +0800

    Add files via upload

---
+++ SW1_no4.py
@@ -0,0 +1,9 @@
+def four():
+    print("How many students are currently enrolled?")
+    stud = int(input())
+    print("How many teachers are currently employed in the school?")
+    prof = int(input())
+    result = stud / prof
+    print("Then the students per teacher is ", result)
+    
+four()
